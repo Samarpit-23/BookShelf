03e9c350a98420639f09ac9d24e668d23488909c
Samarpit-23 <138994697+Samarpit-23@users.noreply.github.com>
2024-11-28 23:53:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const app = express();
 // app.use(cors());
 app.use(cors(
   {
-    origin: ["https://book-shelf-54vp.vercel.app"],
+    origin: ["https://book-shelf-54vp.vercel.app","http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   }
 ));
