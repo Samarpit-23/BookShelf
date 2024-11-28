a8f97c2a45b2e8a55a80b0dbda7708362af7263e
Samarpit-23 <138994697+Samarpit-23@users.noreply.github.com>
2024-11-28 21:54:03 +0530

    Update index.js: Import axios

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import { AuthProvider } from "./context/auth";
 import { SearchProvider } from "./context/search";
 import "antd/dist/reset.css";
 import { CartProvider } from "./context/cart";
+import axios from "axios";
 
 axios.defaults.baseURL = 'https://book-shelf-cc2nay3w2-samars-projects-c658d474.vercel.app';
 const root = ReactDOM.createRoot(document.getElementById("root"));
